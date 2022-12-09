9d2db2a721d73f5be87df312d9f4d6b4811fc12c
Erudito <81595290+theerudito@users.noreply.github.com>
2022-12-09 13:48:50 -0500

    first commit

--- Consultas/ConsultasNaruto.cs
+++ Consultas/ConsultasNaruto.cs
@@ -6,6 +6,7 @@ namespace MySQL_APP
   internal class ConsultasNaruto
   {
     connectionMySQL conexion = new connectionMySQL();
+
     public void getAllCharacters()
     {
       conexion.openConection();
@@ -17,6 +18,50 @@ namespace MySQL_APP
         Console.WriteLine(reader["id"] + " - " + reader["name"] + " - " + reader["clan"] + " - " + reader["age"]);
       }
       conexion.closeConection();
+    }
+
+    public void getCharacter(int id)
+    {
+      conexion.openConection();
+      string query = $"SELECT * FROM Naruto.characters WHERE id = {id}";
+      MySqlCommand command = new MySqlCommand(query, conexion.openConection());
+      MySqlDataReader reader = command.ExecuteReader();
+      while (reader.Read())
+      {
+        Console.WriteLine(reader["id"] + " - " + reader["name"] + " - " + reader["clan"] + " - " + reader["age"]);
+      }
+      conexion.closeConection();
+    }
+
+    public void insertCharacter(Naruto character)
+    {
+      conexion.openConection();
+      string query = $"INSERT INTO Naruto.characters (name, clan, age) VALUES ('{character.name}', '{character.clan}', '{character.age}')";
+      MySqlCommand command = new MySqlCommand(query, conexion.openConection());
+      command.ExecuteNonQuery();
+      Console.WriteLine("Registro insertado");
+      conexion.closeConection();
+    }
+
+    public void updateCharacter(Naruto character)
+    {
+      conexion.openConection();
+      string query = $"UPDATE Naruto.characters SET name = '{character.name}', clan = '{character.clan}', age = '{character.age}' WHERE id = {character.id}";
+      MySqlCommand command = new MySqlCommand(query, conexion.openConection());
+      command.ExecuteNonQuery();
+      Console.WriteLine("Registro actualizado");
+      conexion.closeConection();
+    }
+
+    public void deleteCharacter(int id)
+    {
+      conexion.openConection();
+      string query = $"DELETE FROM Naruto.characters WHERE id = {id}";
+      MySqlCommand command = new MySqlCommand(query, conexion.openConection());
+      command.ExecuteNonQuery();
+      Console.WriteLine("Registro eliminado");
+      conexion.closeConection();
+
     }
   }
 }
--- Program.cs
+++ Program.cs
@@ -5,7 +5,20 @@
     static void Main(string[] args)
     {
       ConsultasNaruto consulta = new ConsultasNaruto();
+      // GET ALL CHARACTERS
       consulta.getAllCharacters();
+
+      // GET CHARACTER BY ID
+      consulta.getCharacter(2);
+
+      // INSERT CHARACTER
+      consulta.insertCharacter(new Naruto { name = "Sarawer", clan = "Uchweriha", age = "15" });
+
+      // UPDATE CHARACTER
+      consulta.updateCharacter(new Naruto { id = 7, name = "Sarada", clan = "Uchiha", age = "10" });
+    
+      // DELETE CHARACTER
+      consulta.deleteCharacter(8);
     } 
   }
 }
